1acd801707a2c0051eb5f3e0de275420dca68ea4
Stijn Robben <stijn.robben2005@gmail.com>
2023-11-07 11:58:40 +0100

    Interceptor

--- apps/data-api/src/main.ts
+++ apps/data-api/src/main.ts
@@ -5,19 +5,21 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ApiResponseInterceptor } from '@avans-nx-workshop/backend/dto';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `data-api 🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+
+  const corsOptions: CorsOptions = {};
+  app.enableCors(corsOptions);
+
+  app.useGlobalInterceptors(new ApiResponseInterceptor());
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
@@ -27,6 +29,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application (data-api) is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
-
 bootstrap();
