5cd455f2c358eae4de52519fb7415ece43f68cb7
Stijn Robben <stijn.robben2005@gmail.com>
2023-11-21 10:50:25 +0100

    local

--- libs/share-a-meal/features/src/lib/menuitem/menuitem.service.ts
+++ libs/share-a-meal/features/src/lib/menuitem/menuitem.service.ts
@@ -18,7 +18,7 @@ export const httpOptions = {
  */
 @Injectable()
 export class MenuItemService {
-  endpoint = 'https://cwfr-stijn-robben.azurewebsites.net/api/menuitem';
+  endpoint = 'http://localhost:3000/api/menuitem';
 
   constructor(private readonly http: HttpClient) {}
 
